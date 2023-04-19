2f3a0f0ec335183cd347248e9d6ab95c7079cbfd
Priyanshu9898 <priyanshumalaviya17@gmail.com>
2023-04-19 10:35:03 +0530

    frontend change

--- frontend/src/utils/ApiRequest.js
+++ frontend/src/utils/ApiRequest.js
@@ -1,4 +1,4 @@
-const host = "http://localhost:5000";
+const host = "https://expense-tracker-app-knl1.onrender.com";
 
 export const setAvatarAPI = `${host}/api/auth/setAvatar`;
 export const registerAPI = `${host}/api/auth/register`;
