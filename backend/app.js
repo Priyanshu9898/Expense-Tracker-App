be7503fe05241bb510cf2d3283d995f10c646e3c
Priyanshu9898 <priyanshu.mce20@sot.pdpu.ac.in>
2023-10-13 15:09:22 +0530

    frontend url added with CORS

--- backend/app.js
+++ backend/app.js
@@ -20,7 +20,7 @@ connectDB();
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://main.d1sj7cd70hlter.amplifyapp.com",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
--- frontend/src/utils/ApiRequest.js
+++ frontend/src/utils/ApiRequest.js
@@ -1,5 +1,5 @@
-// const host = "https://expense-tracker-app-knl1.onrender.com";
-const host = "http://localhost:5000";
+const host = "https://expense-tracker-app-knl1.onrender.com";
+// const host = "http://localhost:5000";
 export const setAvatarAPI = `${host}/api/auth/setAvatar`;
 export const registerAPI = `${host}/api/auth/register`;
 export const loginAPI = `${host}/api/auth/login`;
